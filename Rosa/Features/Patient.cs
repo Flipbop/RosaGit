59a592a5478f57afdb446c75c5b1656e005521cc
Flipbop <151550611+Flipbop@users.noreply.github.com>
2025-09-04 07:55:19 -0600

    0.0.1

--- Rosa/Features/Patient.cs
+++ Rosa/Features/Patient.cs
@@ -1,5 +1,7 @@
 ﻿using Nickel;
 using System.Linq;
+using System.Reflection;
+using HarmonyLib;
 
 namespace Flipbop.Rosa;
 
@@ -19,9 +21,21 @@ internal sealed class PatientManager
 	
 	public PatientManager()
 	{
-		ModEntry.Instance.Helper.Events.RegisterAfterArtifactsHook(nameof(Artifact.OnTurnEnd), (State state, Combat combat) =>
+		ModEntry.Instance.Harmony.Patch(
+			original: AccessTools.DeclaredMethod(typeof(AEndTurn), nameof(AEndTurn.Begin)),
+			prefix: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(AEndTurn_Begin_Prefix))
+		);
+		/*ModEntry.Instance.Helper.Events.RegisterAfterArtifactsHook(nameof(Artifact.OnTurnEnd), (State state, Combat combat) =>
 		{
 			foreach (Card card in combat.discard)
+			{
+				if (card.GetIsPatient() && !ModEntry.Instance.Helper.ModData.GetModDataOrDefault<bool>(card, "PatientTick")) 
+				{ 
+					card.discount -= 1; 
+					ModEntry.Instance.Helper.ModData.SetModData(card, "PatientTick", true);
+				}
+			}
+			foreach (Card card in state.deck)
 			{
 				if (card.GetIsPatient() && !ModEntry.Instance.Helper.ModData.GetModDataOrDefault<bool>(card, "PatientTick")) 
 				{ 
@@ -47,6 +61,17 @@ internal sealed class PatientManager
 			(State state) =>
 			{
 				foreach (Card card in state.deck) ModEntry.Instance.Helper.ModData.SetModData(card, "PatientTick", false);
-			});
+			});*/
+	}
+	
+	private static void AEndTurn_Begin_Prefix(State s, Combat c)
+	{
+		if (c.cardActions.Any(a => a is AEndTurn))
+			return;
+		foreach (Card card in c.hand)
+		{
+			if (card.GetIsPatient()) card.discount -= 1;
+		} 
+
 	}
 }
