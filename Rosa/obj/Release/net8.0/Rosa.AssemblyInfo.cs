2c824e577c0ed969cae3d6af613be15e27e66c1f
Flipbop <151550611+Flipbop@users.noreply.github.com>
2025-09-11 09:48:08 -0600

    1.0.0

--- Rosa/obj/Release/net8.0/Rosa.AssemblyInfo.cs
+++ Rosa/obj/Release/net8.0/Rosa.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rosa")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c3cfcf42d04db2c20515323884c21615c89961c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rosa")]
